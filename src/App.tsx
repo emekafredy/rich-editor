b8726377287432158546c7251446695a961056ed
emekafredy <emeka.s.chinedu@gmail.com>
2023-12-02 22:13:11 +0100

    set up with vite(react) and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import RichTextEditor from "./views/RichTextEditor";
+
+function App() {
+  return (
+    <div className="h-screen bg-white">
+      <RichTextEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      white: {
+        100: "#FFFFFF",
+        200: "#FAFAFA",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
